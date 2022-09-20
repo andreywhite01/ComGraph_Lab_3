3ee59303a5a93974cef42c45ef5b56a76eaa13e3
Андрей Кандыбо <andreykandybo@gmail.com>
2022-09-20 21:10:58 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace ComGraph_Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRGBToGray = new System.Windows.Forms.Button();
+            this.btnPrewittX = new System.Windows.Forms.Button();
+            this.btnSobelX = new System.Windows.Forms.Button();
+            this.tbThreshold = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBarThreshold = new System.Windows.Forms.TrackBar();
+            this.btnPrewittY = new System.Windows.Forms.Button();
+            this.btnSobelY = new System.Windows.Forms.Button();
+            this.btnToOriginal = new System.Windows.Forms.Button();
+            this.btnLaplacce = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarThreshold)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 497);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(28, 22);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Открыть";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(28, 67);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRGBToGray
+            // 
+            this.btnRGBToGray.Location = new System.Drawing.Point(28, 347);
+            this.btnRGBToGray.Name = "btnRGBToGray";
+            this.btnRGBToGray.Size = new System.Drawing.Size(75, 37);
+            this.btnRGBToGray.TabIndex = 3;
+            this.btnRGBToGray.Text = "Градация серого";
+            this.btnRGBToGray.UseVisualStyleBackColor = true;
+            this.btnRGBToGray.Click += new System.EventHandler(this.btnRGBToGray_Click);
+            // 
+            // btnPrewittX
+            // 
+            this.btnPrewittX.Location = new System.Drawing.Point(109, 301);
+            this.btnPrewittX.Name = "btnPrewittX";
+            this.btnPrewittX.Size = new System.Drawing.Size(75, 48);
+            this.btnPrewittX.TabIndex = 4;
+            this.btnPrewittX.Text = "Фильтр Превитта по x";
+            this.btnPrewittX.UseVisualStyleBackColor = true;
+            this.btnPrewittX.Click += new System.EventHandler(this.btnPrewittX_Click);
+            // 
+            // btnSobelX
+            // 
+            this.btnSobelX.Location = new System.Drawing.Point(109, 355);
+            this.btnSobelX.Name = "btnSobelX";
+            this.btnSobelX.Size = new System.Drawing.Size(75, 37);
+            this.btnSobelX.TabIndex = 5;
+            this.btnSobelX.Text = "Фильтр Собеля по x";
+            this.btnSobelX.UseVisualStyleBackColor = true;
+            this.btnSobelX.Click += new System.EventHandler(this.btnSobelX_Click);
+            // 
+            // tbThreshold
+            // 
+            this.tbThreshold.Enabled = false;
+            this.tbThreshold.Location = new System.Drawing.Point(82, 244);
+            this.tbThreshold.Name = "tbThreshold";
+            this.tbThreshold.Size = new System.Drawing.Size(47, 20);
+            this.tbThreshold.TabIndex = 6;
+            this.tbThreshold.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Порог:";
+            // 
+            // trackBarThreshold
+            // 
+            this.trackBarThreshold.Location = new System.Drawing.Point(135, 235);
+            this.trackBarThreshold.Maximum = 255;
+            this.trackBarThreshold.Name = "trackBarThreshold";
+            this.trackBarThreshold.Size = new System.Drawing.Size(175, 45);
+            this.trackBarThreshold.TabIndex = 8;
+            this.trackBarThreshold.Value = 100;
+            this.trackBarThreshold.Scroll += new System.EventHandler(this.refreshThreshold);
+            // 
+            // btnPrewittY
+            // 
+            this.btnPrewittY.Location = new System.Drawing.Point(190, 301);
+            this.btnPrewittY.Name = "btnPrewittY";
+            this.btnPrewittY.Size = new System.Drawing.Size(75, 48);
+            this.btnPrewittY.TabIndex = 9;
+            this.btnPrewittY.Text = "Фильтр Превитта по y";
+            this.btnPrewittY.UseVisualStyleBackColor = true;
+            this.btnPrewittY.Click += new System.EventHandler(this.btnPrewittY_Click);
+            // 
+            // btnSobelY
+            // 
+            this.btnSobelY.Location = new System.Drawing.Point(190, 355);
+            this.btnSobelY.Name = "btnSobelY";
+            this.btnSobelY.Size = new System.Drawing.Size(75, 37);
+            this.btnSobelY.TabIndex = 10;
+            this.btnSobelY.Text = "Фильтр Собеля по y";
+            this.btnSobelY.UseVisualStyleBackColor = true;
+            this.btnSobelY.Click += new System.EventHandler(this.btnSobelY_Click);
+            // 
+            // btnToOriginal
+            // 
+            this.btnToOriginal.Location = new System.Drawing.Point(150, 44);
+            this.btnToOriginal.Name = "btnToOriginal";
+            this.btnToOriginal.Size = new System.Drawing.Size(101, 23);
+            this.btnToOriginal.TabIndex = 11;
+            this.btnToOriginal.Text = "К оригиналу";
+            this.btnToOriginal.UseVisualStyleBackColor = true;
+            this.btnToOriginal.Click += new System.EventHandler(this.toOriginal);
+            // 
+            // btnLaplacce
+            // 
+            this.btnLaplacce.Location = new System.Drawing.Point(154, 398);
+            this.btnLaplacce.Name = "btnLaplacce";
+            this.btnLaplacce.Size = new System.Drawing.Size(75, 42);
+            this.btnLaplacce.TabIndex = 12;
+            this.btnLaplacce.Text = "Оператор Лапласса";
+            this.btnLaplacce.UseVisualStyleBackColor = true;
+            this.btnLaplacce.Click += new System.EventHandler(this.btnLaplacce_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 605);
+            this.Controls.Add(this.btnLaplacce);
+            this.Controls.Add(this.btnToOriginal);
+            this.Controls.Add(this.btnSobelY);
+            this.Controls.Add(this.btnPrewittY);
+            this.Controls.Add(this.trackBarThreshold);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbThreshold);
+            this.Controls.Add(this.btnSobelX);
+            this.Controls.Add(this.btnPrewittX);
+            this.Controls.Add(this.btnRGBToGray);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarThreshold)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRGBToGray;
+        private System.Windows.Forms.Button btnPrewittX;
+        private System.Windows.Forms.Button btnSobelX;
+        private System.Windows.Forms.TextBox tbThreshold;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBarThreshold;
+        private System.Windows.Forms.Button btnPrewittY;
+        private System.Windows.Forms.Button btnSobelY;
+        private System.Windows.Forms.Button btnToOriginal;
+        private System.Windows.Forms.Button btnLaplacce;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace ComGraph_Lab_3
+{  
+
+
+    public partial class Form1 : Form
+    { //Объявляем переменные доступные в каждом обработчике события
+        private Point PreviousPoint, point; //Точка до перемещения курсора мыши
+                                            //и текущая точка
+        private Bitmap bmp;
+        private Pen blackPen;
+        private Graphics g;
+        private Image originalImage = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            blackPen = new Pen(Color.Black, 4); //подготавливаем перо
+        }
+
+        private void repleceObject(Control controller, int x)
+        {
+            controller.Location = new Point(controller.Location.X + x, controller.Location.Y);
+        }
+        private void replaceObjects(int x)
+        {
+            repleceObject(btnToOriginal, x);
+            repleceObject(btnOpen, x);
+            repleceObject(btnSave, x);
+            repleceObject(btnPrewittX, x);
+            repleceObject(btnPrewittY, x);
+            repleceObject(btnSobelX, x);
+            repleceObject(btnSobelY, x);
+            repleceObject(btnRGBToGray, x);
+            repleceObject(btnLaplacce, x);
+
+            repleceObject(trackBarThreshold, x);
+
+            repleceObject(label1, x);
+            repleceObject(tbThreshold, x);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        { //открытие файла
+            OpenFileDialog dialog = new OpenFileDialog();
+            //задаем расширения файлов
+            dialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)| *.bmp; *.jpg; *.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+            if (dialog.ShowDialog() == DialogResult.OK)//вызываем диалоговое окно
+            {
+                int currentPictureBoxWidth = pictureBox1.Width;
+
+
+                originalImage = Image.FromFile(dialog.FileName); //Загружаем в image
+                                                               //изображение из выбранного файла
+                int width = originalImage.Width;
+                int height = originalImage.Height;
+                pictureBox1.Width = width;
+                pictureBox1.Height = height;
+                
+                bmp = new Bitmap(originalImage, width, height); //создаем и загружаем из
+                                                        //image изображение в формате bmp
+                pictureBox1.Image = bmp; //записываем изображение в формате bmp
+                                         //в pictureBox1
+                g = Graphics.FromImage(pictureBox1.Image); //подготавливаем объект
+                                                           //Graphics для рисования в pictureBox1
+                replaceObjects(width - currentPictureBoxWidth);
+            }
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        { // обработчик события нажатия кнопки на мыши
+          // записываем в предыдущую точку (PreviousPoint) текущие координаты
+            PreviousPoint.X = e.X;
+            PreviousPoint.Y = e.Y;
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {//Обработчик события перемещения мыши по pictuteBox1
+            if (e.Button == MouseButtons.Left) //Проверяем нажатие левой кнопка
+            { //запоминаем в point текущее положение курсора мыши
+                point.X = e.X;
+                point.Y = e.Y;
+                //соединяем линией предыдущую точку с текущей
+                g.DrawLine(blackPen, PreviousPoint, point);
+                //текущее положение курсора мыши сохраняем в PreviousPoint
+                PreviousPoint.X = point.X;
+                PreviousPoint.Y = point.Y;
+                pictureBox1.Invalidate();//Принудительно вызываем перерисовку
+            }
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        { //сохранение файла
+            SaveFileDialog savedialog = new SaveFileDialog();
+            //задаем свойства для savedialog
+            savedialog.Title = "Сохранить картинку как ...";
+            savedialog.OverwritePrompt = true;
+            savedialog.CheckPathExists = true;
+            savedialog.Filter =
+            "Bitmap File(*.bmp)|*.bmp|" +
+            "GIF File(*.gif)|*.gif|" +
+            "JPEG File(*.jpg)|*.jpg|" +
+            "TIF File(*.tif)|*.tif|" +
+            "PNG File(*.png)|*.png";
+            savedialog.ShowHelp = true;
+            if (savedialog.ShowDialog() == DialogResult.OK)
+            {
+                // в fileName записываем полный путь к файлу
+                string fileName = savedialog.FileName;
+                // Убираем из имени три последних символа (расширение файла)
+                string strFilExtn =
+                fileName.Remove(0, fileName.Length - 3);
+                // Сохраняем файл в нужном формате и с нужным расширением
+                switch (strFilExtn)
+                {
+                    case "bmp":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case "jpg":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "gif":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                    case "tif":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                    case "png":
+                        bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void RGBToGray()
+        {
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    int R = bmp.GetPixel(i, j).R; //извлекаем долю красного цвета
+                    int G = bmp.GetPixel(i, j).G; //извлекаем долю зеленого цвета
+                    int B = bmp.GetPixel(i, j).B; //извлекаем долю синего цвета
+                    int Gray = (R + G + B) / 3; // высчитываем среднее
+                    Color p = Color.FromArgb(255, Gray, Gray, Gray);
+
+                    bmp.SetPixel(i, j, p); //записываем полученный цвет в точку
+                }
+        }
+
+        private void btnRGBToGray_Click(object sender, EventArgs e)
+        { //циклы для перебора всех пикселей на изображении
+            RGBToGray();
+            Refresh(); //вызываем функцию перерисовки окна
+        }
+
+        private void applyFilter(float[,] ker)
+        {
+            Bitmap newBmp = new Bitmap(bmp);
+            Color p;
+            int threshold = trackBarThreshold.Value;
+
+            { //циклы для перебора всех пикселей на изображении
+                for (int row = 0; row < bmp.Width; row++)
+                    for (int col = 0; col < bmp.Height; col++)
+                    {
+                        //применяем ядро, если оно не выходит за границы изображения
+                        if (row > 0 && row < bmp.Width - 1 && col > 0 && col < bmp.Height - 1)
+                        {
+                            int newValue = 0;
+
+                            //циклы для перебора значений ядра со значениями в пикселях вокруг текущего
+                            for (int kerRow = 0; kerRow < 3; ++kerRow)
+                                for (int kerCol = 0; kerCol < 3; ++kerCol)
+                                {
+                                    int grey = bmp.GetPixel(row - 1 + kerRow, col - 1 + kerCol).R; //извлекаем долю серого
+                                    
+                                    int grad = (int)(ker[kerRow, kerCol] * (grey));
+
+                                    newValue += grad;
+                                }
+
+                            newValue = (int)Math.Abs(newValue) > threshold ? 250 : 0;
+
+                            p = Color.FromArgb(255, newValue, newValue, newValue);
+
+                            newBmp.SetPixel(row, col, p); //записываем полученный цвет в точку
+
+                        }
+                        else
+                        {
+                            int R = bmp.GetPixel(row, col).R; //извлекаем долю красного цвета
+                            int Gray = (R) / 3; // высчитываем среднее
+                            p = Color.FromArgb(255, Gray, Gray, Gray);
+
+                            newBmp.SetPixel(row, col, p); //записываем полученный цвет в точку
+                        }
+
+                    }
+
+                //циклы для перебора всех пикселей на изображении
+                for (int row = 0; row < bmp.Width; row++)
+                    for (int col = 0; col < bmp.Height; col++)
+                    {
+                        int newValue = newBmp.GetPixel(row, col).R;
+                        p = Color.FromArgb(255, newValue, newValue, newValue);
+
+                        bmp.SetPixel(row, col, p);
+                    }
+                Refresh(); //вызываем функцию перерисовки окна
+            }
+        }
+
+        private void refreshThreshold(object sender, EventArgs e)
+        {
+            tbThreshold.Text = string.Format("{0}", trackBarThreshold.Value);
+        }
+
+        private void btnPrewittX_Click(object sender, EventArgs e)
+        {
+            RGBToGray();
+            float[,] ker = {
+                { -1f, 0f, 1f },
+                { -1f, 0f, 1f },
+                { -1f, 0f, 1f } };
+            applyFilter(ker);
+        }
+
+        private void btnPrewittY_Click(object sender, EventArgs e)
+        {
+            RGBToGray();
+            float[,] ker = {
+                { -1f, -1f, -1f },
+                { 0f, 0f, 0f },
+                { 1f, 1f, 1f } };
+            applyFilter(ker);
+        }
+
+        private void btnSobelX_Click(object sender, EventArgs e)
+        {
+            RGBToGray();
+            float[,] ker = {
+                { -1f, 0f, 1f },
+                { -2f, 0f, 2f },
+                { -1f, 0f, 1f } };
+            applyFilter(ker);
+        }
+
+
+        private void btnSobelY_Click(object sender, EventArgs e)
+        {
+            RGBToGray();
+
+            float[,] ker = {
+                { -1f, -1f, -1f },
+                { 0f, 0f, 0f },
+                { 1f, 2f, 1f } };
+            applyFilter(ker);
+
+        }
+
+        private void btnLaplacce_Click(object sender, EventArgs e)
+        {
+            RGBToGray();
+
+            float[,] ker = {
+                { -1f, -1f, -1f },
+                { -1f, 8f, -1f },
+                { -1f, -1f, -1f } };
+            applyFilter(ker);
+        }
+
+        private void toOriginal(object sender, EventArgs e)
+        {
+            pictureBox1.Image = originalImage;
+            bmp = new Bitmap(originalImage, pictureBox1.Width, pictureBox1.Height);
+
+            pictureBox1.Image = bmp;
+
+            g = Graphics.FromImage(pictureBox1.Image);
+
+        }
+    }
+}
